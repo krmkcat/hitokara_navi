b9eeb49b8952eae061d73f1d0954cc5b1d4aef24
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-03-03 15:54:14 +0900

    edit:会員登録ページのルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: %i[new create]
+  resources :users, only: :create
+  get 'signup', to: 'users#new'
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
