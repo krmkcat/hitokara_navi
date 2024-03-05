af6a39db71706436cd5014aff5cb618a169294ed
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-03-05 20:49:06 +0900

    fix:ログイン時のフラッシュメッセージを修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,9 +7,9 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
     if @user
       # redirect_back_or_to shops_path, success: t('.success')
-      redirect_to root_path, success: '会員登録に成功しました' # 仮の処理。ショップ一覧ページができたら↑に替える
+      redirect_to root_path, success: 'ログインに成功しました' # 仮の処理。ショップ一覧ページができたら↑に替える
     else
-      flash.now[:error] = '会員登録に失敗しました'
+      flash.now[:error] = 'ログインに失敗しました'
       render :new, status: :unprocessable_entity
     end
   end
