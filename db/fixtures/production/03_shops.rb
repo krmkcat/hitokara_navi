657bffc6ecdd4c250dcef50956e5e73cf813ab46
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-03-09 17:44:04 +0900

    add:本番環境用の仮ダミーデータを作成

---
+++ db/fixtures/production/03_shops.rb
@@ -0,0 +1,32 @@
+Shop.seed(
+  { id: 1,
+    area_id: 1,
+    name: 'ニャンカラ栄店',
+    address: 'キャットタワー3F',
+    phone_number: '052-111-1111',
+    url: 'https://nyankara.com/sakae',
+    opening_hours: '11:00-24:00',
+    latitude: 1.1,
+    longitude: 1.1
+  },
+  { id: 2,
+    area_id: 2,
+    name: 'ニャンカラ名古屋駅前店',
+    address: '段ボールの中',
+    phone_number: '052-222-2222',
+    url: 'https://nyankara.com/meieki',
+    opening_hours: '11:00-24:00',
+    latitude: 1.1,
+    longitude: 1.1
+  },
+  { id: 3,
+    area_id: 1,
+    name: 'カラオケWanWan栄店',
+    address: 'ドッグランの真ん中',
+    phone_number: '052-333-3333',
+    url: 'https://karaoke-wanwan.com/sakae',
+    opening_hours: '9:00-21:00',
+    latitude: 1.1,
+    longitude: 1.1
+  }
+  )
