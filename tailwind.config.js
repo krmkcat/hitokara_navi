d0ab3e1a81e86caac086add49b6de326440f5d54
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-03-10 12:19:51 +0900

    edit:daisyUIのカスタムテーマを設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,4 +12,21 @@ module.exports = {
     // 他の動的に生成されるクラス名もここに追加
   ],
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#3F6C45",
+          "secondary": "#50312F",
+          "accent": "#CB0000",
+          "neutral": "#fef9c3",
+          "base-100": "#E4EA8C",
+          "info": "#5eead4",
+          "success": "#4ade80",
+          "warning": "#fbbf24",
+          "error": "#f87171",
+        },
+      },
+    ],
+  },
 }
