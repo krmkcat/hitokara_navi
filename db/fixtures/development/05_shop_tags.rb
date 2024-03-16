290b7e2f06e892962447904b5ec3d63fc4c42c34
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-03-16 14:31:45 +0900

    add:店舗一覧及び詳細にタグを表示

---
+++ db/fixtures/development/05_shop_tags.rb
@@ -0,0 +1,8 @@
+ShopTag.seed(:shop_id, :tag_id,
+  { shop_id: 1, tag_id: 1 },
+  { shop_id: 1, tag_id: 2 },
+  { shop_id: 1, tag_id: 3 },
+  { shop_id: 2, tag_id: 4 },
+  { shop_id: 2, tag_id: 2 },
+  { shop_id: 2, tag_id: 1 }
+)
