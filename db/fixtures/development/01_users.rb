775e8139fcb1e984c58154bb849c808e879ddcff
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-03-17 21:39:48 +0900

    edit:userおよびprofileのseedファイルを作り直し、seedファイルを整理

--- db/fixtures/development/01_users.rb
+++
@@ -1,8 +0,0 @@
-User.seed(:email,
-  { email: 'admin@test', password: 'password', password_confirmation: 'password', role: :admin },
-  { email: 'general1@test', password: 'password', password_confirmation: 'password', role: :general },
-  { email: 'general2@test', password: 'password', password_confirmation: 'password', role: :general },
-  { email: 'general3@test', password: 'password', password_confirmation: 'password', role: :general },
-  { email: 'general4@test', password: 'password', password_confirmation: 'password', role: :general },
-  { email: 'general5@test', password: 'password', password_confirmation: 'password', role: :general }
-)
