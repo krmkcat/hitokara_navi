b1e7071334fbce25453419441c13435d412d42f9
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-03-23 16:25:47 +0900

    add:フォームオブジェクトを作成しワード検索機能を実装

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,12 +2,18 @@ class ShopsController < ApplicationController
   skip_before_action :require_login
 
   def index
-    @q = Shop.ransack(params[:q])
-    @shops = @q.result(distinct: true)
+    @search_shops_form = SearchShopsForm.new(search_params)
+    @shops = @search_shops_form.search.includes(:tags)
   end
 
   def show
     @shop = Shop.find(params[:id])
     @reviews = @shop.reviews.order(updated_at: :desc).limit(5)
+  end
+
+  private
+
+  def search_params
+    params[:q]&.permit(%i[words int_average eqcust_average sofr_average])
   end
 end
