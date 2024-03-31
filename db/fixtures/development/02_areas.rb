14ee45a46f45d0849d26e60901ff1752e090ee90
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-03-31 19:04:57 +0900

    edit:areaのseed_fuファイルを編集

--- db/fixtures/development/02_areas.rb
+++ db/fixtures/development/02_areas.rb
@@ -5,11 +5,10 @@ Area.seed(
   { id: 4, prefecture_id: 22, name: '名古屋 東部' },
   { id: 5, prefecture_id: 22, name: '名古屋 西部' },
   { id: 6, prefecture_id: 22, name: '名古屋 南部' },
-  { id: 7, prefecture_id: 22, name: '名古屋 北部' },
+  { id: 7, prefecture_id: 22, name: '名古屋近郊' },
   { id: 8, prefecture_id: 22, name: '尾張北部' },
   { id: 9, prefecture_id: 22, name: '尾張東部' },
   { id: 10, prefecture_id: 22, name: '西三河' },
   { id: 11, prefecture_id: 22, name: '東三河' },
-  { id: 12, prefecture_id: 22, name: '知多' },
-  { id: 13, prefecture_id: 13, name: '渋谷区' }
+  { id: 12, prefecture_id: 22, name: '知多エリア' },
 )
