ac9795e205409a901521e02d85f143f2e6d0f5ce
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-04-04 18:30:17 +0900

    edit:アプリ全体のカラーテーマを変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,20 +13,6 @@ module.exports = {
   ],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          "primary": "#3F6C45",
-          "secondary": "#50312F",
-          "accent": "#CB0000",
-          "neutral": "#fef9c3",
-          "base-100": "#E4EA8C",
-          "info": "#5eead4",
-          "success": "#4ade80",
-          "warning": "#fbbf24",
-          "error": "#f87171",
-        },
-      },
-    ],
+    themes: ['lemonade'],
   },
 }
