56d93c03c8a3ea2644da31944779ac9d1b5f022c
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-04-07 21:32:45 +0900

    fix:OGPまわりのコードを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,17 @@
 module ApplicationHelper
-  def show_meta_tags
-    assign_meta_tags if display_meta_tags.blank?
-    display_meta_tags
-  end
-
   def default_meta_tags
     {
       site: 'ヒトカラナビ',
       title: 'ヒトカラー向けのカラオケ店検索＆レビューサービス',
       reverse: true,
       separator: '|', # Webサイト名とページタイトルを区切るために使用されるテキスト
-      description: 'ヒトカラ好きならではの目線でカラオケ店の評価を共有し、あなたのこだわりにあったお店をみつけましょう。',
+      description: 'ヒトカラー目線でカラオケ店の評価を共有し、あなたのこだわりにあったお店をみつけましょう。',
       keywords: 'ヒトカラ,ひとりカラオケ,一人カラオケ', # キーワードを「,」区切りで設定する
       canonical: request.original_url, # 優先するurlを指定する
       noindex: !Rails.env.production?,
-      # icon: [                    #favicon、apple用アイコンを指定する
+      # icon: [ #favicon、apple用アイコンを指定する
       #   { href: image_url('favicon.ico') },
-      #   { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      #   { href: image_url('icon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
       # ],
       og: {
         site_name: :site,
