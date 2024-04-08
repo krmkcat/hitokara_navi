d8700873aef3d990fea7b08e31fd62e8d1ee1e7d
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-04-08 22:30:44 +0900

    add:ユーザー管理機能を実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,40 @@
 class Admin::UsersController < Admin::BaseController
   def index
-  end
-
-  def new
+    @users = User.all.includes(:profile).order(created_at: :desc)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), success: t('defaults.flash_message.updated', item: User.model_name.human)
+    else
+      flash.now[:error] = t('defaults.flash_message.not_updated', item: User.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy!
+    if request.referer&.include?('/admin/users/index')
+      flash.now[:success] = t('defaults.flash_message.deleted', item: User.model_name.human)
+      render :destroy
+    else
+      redirect_to admin_users_path, status: :see_other, success: t('defaults.flash_message.deleted', item: User.model_name.human)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :role, :password, :password_confirmation)
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,6 @@
 class Tag < ApplicationRecord
   has_many :shop_tags
-  has_many :review_tags
   has_many :shops, through: :shop_tags, dependent: :destroy
-  has_many :reviews, through: :review_tags, dependent: :destroy
 
   validates :name, presence: true
 end
