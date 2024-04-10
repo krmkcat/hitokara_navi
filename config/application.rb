e1a252eea4391e2e9270de44e8d99c18861b1f2e
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-04-10 12:11:43 +0900

    edit:タイムゾーンの設定を追加

--- config/application.rb
+++ config/application.rb
@@ -15,12 +15,22 @@ module App
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+
+    # タイムゾーンを東京に設定
+    config.time_zone = 'Tokyo'
+    # DB読み書き時のタイムゾーンを実行環境のOSと同じに設定
+    config.active_record.default_timezone= :local
+
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     config.action_mailer.default_url_options = Settings.default_url_options.to_h
+
+    config.generators do |g|
+      g.helper false # helperを生成しない
+      g.test_framework false #testを生成しない
+      g.skip_routes true #ルーティングの記述を生成しない
+    end
   end
 end
