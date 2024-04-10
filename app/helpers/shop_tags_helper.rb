de699165a61da1e8888199db23c30ee55bae6ff9
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-04-10 12:17:25 +0900

    delete:不要なファイル（helper, test）を削除

--- app/helpers/shop_tags_helper.rb
+++
@@ -1,2 +0,0 @@
-module ShopTagsHelper
-end
--- test/controllers/admin/dashboards_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Admin::DashboardsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get admin_dashboards_index_url
-    assert_response :success
-  end
-end
