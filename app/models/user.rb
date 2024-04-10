2902b5746b84aee3d8652fecd23d4c722893fa1b
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-04-10 21:59:46 +0900

    edit:入力フォーム関連の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
-  validates :password, length: { minimum: 3 }, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 8 }, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password, presence: true, on: :reset_password
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :email, presence: true, uniqueness: true
