59078b663add87da969253e483f7d27c8a362773
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-04-27 14:31:50 +0900

    edit:shop_locations_paramsを定義

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,7 +2,8 @@ class ShopsController < ApplicationController
   skip_before_action :require_login
 
   def index
-    @search_shops_form = SearchShopsForm.new(search_params)
+    @shop_locations_form = ShopLocationsForm.new(shop_locations_params)
+    @search_shops_form = SearchShopsForm.new(shop_details_params)
     @all_shops = @search_shops_form.search
     @shops = @all_shops.includes(:tags).order(area_id: :asc).page(params[:page])
   end
@@ -13,8 +14,11 @@ class ShopsController < ApplicationController
   end
 
   private
+  def shop_locations_params
+    params[:l_q]&.permit(:words, :area_id, :prefecture_id)
+  end
 
-  def search_params
+  def shop_details_params
     params[:q]&.permit(:words, :int_average, :eqcust_average, :sofr_average, :area_id, :prefecture_id, tag_ids: [])
   end
 end
