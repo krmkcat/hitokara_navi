542a2562e12baa76dd55f0973165941516716fab
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-05-02 22:47:18 +0900

    edit:「現在地から探す」ボタンの調整

--- app/javascript/get_current_location.js
+++ app/javascript/get_current_location.js
@@ -1,39 +1,45 @@
 function geoFindMe() {
-  const status = document.querySelector("#status");
-  const lat_field = document.querySelector("#q_latitude");
-  const lng_filed = document.querySelector("#q_longitude");
+  const warning = document.querySelector("#warning");
+  const latField = document.querySelector("#q_latitude");
+  const lngField = document.querySelector("#q_longitude");
 
   function success(position) {
-    let raw_latitude = position.coords.latitude;
-    raw_latitude = raw_latitude * 1000000;
-    raw_latitude = Math.round(raw_latitude);
-    let raw_longitude = position.coords.longitude;
-    raw_longitude = raw_longitude * 1000000;
-    raw_longitude = Math.round(raw_longitude);
-    const latitude = raw_latitude / 1000000;
-    const longitude = raw_longitude / 1000000;
+    const raw_latitude = position.coords.latitude;
+    const raw_longitude = position.coords.longitude;
+    const latitude = Math.round(raw_latitude * 1000000) / 1000000;
+    const longitude = Math.round(raw_longitude * 1000000) / 1000000;
 
-    lat_field.value = latitude;
-    lng_filed.value = longitude;
+    latField.value = latitude;
+    lngField.value = longitude;
 
-    search_button.textContent = "現在地から探す";
-    search_button.classList.remove("btn-disabled");
     document.getElementById("current-location-form").submit();
   }
 
   function error() {
-    status.textContent = "Unable to retrieve your location";
+    warning.textContent = "位置情報を取得できません";
   }
 
   if (!navigator.geolocation) {
-    status.textContent = "このブラウザーは位置情報に対応していません";
+    warning.textContent = "このブラウザーは位置情報に対応していません";
   } else {
-    search_button.textContent = "位置情報を取得中…";
-    search_button.classList.add("btn-disabled");
+    searchIcon.remove();
+    buttonText.before(loadingSpinner);
+    buttonText.textContent = "位置情報を取得中…";
+    searchButton.classList.add("btn-disabled");
     navigator.geolocation.getCurrentPosition(success, error);
   }
 }
 
-const search_button = document.querySelector("#search-with-current-location");
+const searchButton = document.querySelector("#search-with-current-location");
+const searchIcon = document.querySelector("#search-icon");
+const loadingSpinner = document.createElement('span');
+loadingSpinner.className = "loading loading-spinner";
+const buttonText = document.querySelector("#button-text");
 
-search_button.addEventListener("click", geoFindMe);
+searchButton.addEventListener("click", geoFindMe);
+
+window.addEventListener("pageshow", function(event) {
+  if (event.persisted) {
+      window.location.reload();
+  }
+});
