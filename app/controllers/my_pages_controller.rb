ddc992e59afd93e0c4a82b18058b44429f19d77a
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-06-09 18:37:17 +0900

    add:マイページに投稿レビュー数と獲得Nice数を表示

--- app/controllers/my_pages_controller.rb
+++ app/controllers/my_pages_controller.rb
@@ -1,5 +1,7 @@
 class MyPagesController < ApplicationController
   def index
     @user = current_user
+    reviews_id = @user.reviews.pluck(:id)
+    @nices_count = Nice.where(review_id: reviews_id).length
   end
 end
