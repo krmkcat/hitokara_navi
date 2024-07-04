58b5fb5986f1aa188d8c343a4068e52ec87568cc
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-07-04 23:03:13 +0900

    add&edit:問い合わせ送信時にユーザーにも控えのメールが送られるように変更

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
   def create
     @contact_form = ContactForm.new(contact_params)
     if @contact_form.valid?
-      ContactMailer.with(user_id: @contact_form.user_id, email: @contact_form.email, body: @contact_form.body, inquiry_types: @contact_form.inquiry_types).contact_email.deliver_now
+      send_emails
       redirect_to root_path, success: t('.success')
     else
       flash.now[:error] = t('.failure')
@@ -20,5 +20,11 @@ class ContactsController < ApplicationController
 
   def contact_params
     params.require(:contact_form).permit(:email, :body, :user_id, inquiry_types: [])
+  end
+
+  def send_emails
+    @contact_id = SecureRandom.uuid
+    ContactMailer.with(contact_id: @contact_id, user_id: @contact_form.user_id, email: @contact_form.email, body: @contact_form.body, inquiry_types: @contact_form.inquiry_types).contact_email.deliver_now
+    ContactMailer.with(email: @contact_form.email, contact_id: @contact_id, inquiry_types: @contact_form.inquiry_types, body: @contact_form.body).contact_copy_email.deliver_now
   end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,17 @@
 class ContactMailer < ApplicationMailer
   def contact_email
-    @contact_id = SecureRandom.uuid
+    @contact_id = params[:contact_id]
     @user = User.find_by(id: params[:user_id])
     @email = params[:email]
     @body = params[:body]
     @inquiry_types = params[:inquiry_types]
     mail to: ENV['GMAIL_USERNAME'], subject: "問い合わせ(#{@contact_id})"
+  end
+
+  def contact_copy_email
+    @contact_id = params[:contact_id]
+    @body = params[:body]
+    @inquiry_types = params[:inquiry_types]
+    mail to: params[:email], subject: 'ヒトカラナビ お問い合わせ控え'
   end
 end
